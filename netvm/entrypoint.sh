8ec6d77096a4f61db67ea1532ad8b97dd23b06a9
Guillermo Rodriguez <guillermor@fing.edu.uy>
2020-07-07 10:37:27 -0300

    Upgraded docker image to debian buster, simplified Dockerfile

--- netvm/entrypoint.sh
+++ netvm/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh 
 
-echo "$(date) Entering Entrypoint"
-
-echo "-- Starting OVS"
-service openvswitch-switch start #This command may cause errors that do not interferre with at least 'mn --test pingall'
-ovs-vsctl set-manager ptcp:6640
+service openvswitch-switch start > /dev/null
+ovs-vsctl set-manager ptcp:6640 > /dev/null
 
 bash
 
-echo "-- Stopping OVS"
-service openvswitch-switch stop
-echo "$(date) Leaving Entrypoint" 
+service openvswitch-switch stop > /dev/null
